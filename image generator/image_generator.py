b1b518a58596237b55b14f0f76cf82e97af9bb66
PolskiPiter <piter73piotr@wp.pl>
2023-07-16 20:51:46 +0200

    Write test of the .PPM file

---
+++ image generator/image_generator.py
@@ -0,0 +1,13 @@
+#Generator obrazków
+
+def main():
+    print("P3")
+    print("# Testowy plik \".PPM\"")
+    print("3 3")
+    print("255")
+    print("0 0 0 255 0 0 0 255 0")
+    print("0 0 255 255 255 0 255 0 255")
+    print("0 255 255 255 255 255 0 0 0")
+
+if __name__ == "__main__":
+    main()
