35b41feee896d409f3a715154f0715ebedc3749f
PolskiPiter <piter73piotr@wp.pl>
2023-07-17 22:12:46 +0200

    New write test of the .PPM file

--- image generator/image_generator.py
+++ image generator/image_generator.py
@@ -1,13 +1,33 @@
 #Generator obrazków
 
 def main():
+    
+    resolution = 600, 600
+    
+    red = []
+    green = []
+    blue = []
+
+    for i in range(resolution[0]):
+        red.append([])
+        green.append([])
+        blue.append([])
+        for j in range(resolution[1]):
+            red[i].append(255)
+            green[i].append(100)
+            blue[i].append(50)
+
     print("P3")
     print("# Testowy plik \".PPM\"")
-    print("3 3")
+    print("{} {}".format(resolution[0], resolution[1]))
     print("255")
-    print("0 0 0 255 0 0 0 255 0")
-    print("0 0 255 255 255 0 255 0 255")
-    print("0 255 255 255 255 255 0 0 0")
+    for i in range(resolution[0]):
+        for j in range(resolution[1]):
+            if j < resolution[1] - 1:
+                end_print = " "
+            else:
+                end_print = "\n"
+            print("{} {} {}".format(red[i][j], green[i][j], blue[i][j]), end=end_print)
 
 if __name__ == "__main__":
     main()
