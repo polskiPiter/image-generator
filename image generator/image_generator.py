3d0abd37fd520fd973ab0932ef224941843c2392
PolskiPiter <piter73piotr@wp.pl>
2023-10-08 22:01:41 +0200

    Creating a method of saving a file

--- image generator/image_generator.py
+++ image generator/image_generator.py
@@ -5,21 +5,22 @@ def main():
     resolution = (1920, 1080)
 
     pixel = {
-        "R": 12,
-        "G": 56,
-        "B": 100
+        "R": 128,
+        "G": 0,
+        "B": 255
     }
     
     pixel_array = list([pixel for _ in range(resolution[0])] for _ in range(resolution[1]))
 
-    print("P3")
-    print("# Testowy plik \".PPM\".")
-    print("# Każda linijka jest zarezerwowana dla jednego piksela.")
-    print("{} {}".format(resolution[0], resolution[1]))
-    print("255")
-    for row_of_pixels in pixel_array:
-        for single_pixel in row_of_pixels:
-            print("{} {} {}".format(single_pixel["R"], single_pixel["G"], single_pixel["B"]))
+    with open("D:/Programowanie/Projekty/Python/Image generator/data/Image.ppm", "w+", encoding="utf-8") as image_file:
+        image_file.write("P3\n")
+        image_file.write("# Testowy plik \".PPM\".\n")
+        image_file.write("# Każda linijka jest zarezerwowana dla jednego piksela.\n")
+        image_file.write("{} {}\n".format(resolution[0], resolution[1]))
+        image_file.write("255\n")
+        for row_of_pixels in pixel_array:
+            for single_pixel in row_of_pixels:
+                image_file.write("{} {} {}\n".format(single_pixel["R"], single_pixel["G"], single_pixel["B"]))
 
 if __name__ == "__main__":
     main()
